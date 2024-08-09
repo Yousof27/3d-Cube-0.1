e564b36998312693787c6bed4d10c9a70c909060
Yousof <yousofkamal111@gmail.com>
2024-08-09 15:58:41 +0300

    Solve A Problem In js Code

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,8 @@ let btn = document.querySelector('.turn');
 btn.addEventListener("mouseenter", ()=>{
     setTimeout(() => {
         let mainFace = document.querySelector('.face5');
-        mainFace.style.backgroundImage = `url(../imgs/5.jpg)`;
+        mainFace.classList.add('start')
         mainFace.innerText = '';
     }, 3000);
 })
+
